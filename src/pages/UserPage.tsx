e69987d54656f4228930a67f253d375bdd392edd
angel <angel.munoz@tokiota.com>
2021-10-22 10:58:33 +0200

    another way to change the route

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,11 +2,22 @@ import {
   Component,
 } from "solid-js";
 
+import { useNavigate } from "solid-app-router";
 
 const UserPage: Component = () => {
+  const navigate = useNavigate();
+
+  const goTo = () => {
+    navigate('/');
+  };
   return (
     <div>
       USER PAGE
+      <br />
+      <br />
+      <br />
+      <br />
+      <button onClick={goTo}>Go to Home</button>
     </div>
   );
 };
