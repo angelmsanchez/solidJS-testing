1fc3e3db74a887fc4e58d042f482b9e96b5fb2f4
angel <angel.munoz@tokiota.com>
2021-11-16 09:54:56 +0100

    visualization data solidJs

--- src/pages/FetchPage.tsx
+++ src/pages/FetchPage.tsx
@@ -1,6 +1,10 @@
 import {
   Component,
   createResource,
+  For,
+  Show,
+  Switch,
+  Match,
 } from "solid-js";
 
 const fetchData = () => {
@@ -16,14 +20,53 @@ const FetchPage: Component = () => {
   const [data, { mutate, refetch }] = createResource(fetchData, { initialValue: [] });
 
   return (
-    <div className="fetch-page-container">
-      {data().map(result => (
-        <div>
-          <h1>{result.name}</h1>
-          <img src={result.image} />
-        </div>
-      ))}
-    </div>
+    <>
+      {/* 1-forma-visualización de datos y errores */}
+      <Switch fallback={<div>Loading...</div>}>
+        <Match when={data.error}>
+          <div>Error</div>
+        </Match>
+        <Match when={data.loading}>
+          <div>Loading...</div>
+        </Match>
+        <Match when={data()}>
+          <For each={data()}>
+            {(item: { name: string, image: string }) => (
+              <div>
+                <h1>{item.name}</h1>
+                <img src={item.image} />
+              </div>
+            )}
+          </For>
+        </Match>
+      </Switch>
+
+      {/* 2-forma-visualización de datos y errores */}
+      <Show when={data.loading}>
+        <div>Loading...</div>
+      </Show>
+      <Show when={data.error}>
+        <div>Error</div>
+      </Show>
+      <For each={data()}>
+        {(item: { name: string, image: string }) => (
+          <div>
+            <h1>{item.name}</h1>
+            <img src={item.image} />
+          </div>
+        )}
+      </For>
+
+      {/* 3-forma-visualización de datos sin errores */}
+      <For each={data()} fallback={<div>Loading...</div>}>
+        {(item: { name: string, image: string }) => (
+          <div>
+            <h1>{item.name}</h1>
+            <img src={item.image} />
+          </div>
+        )}
+      </For>
+    </>
   );
 };
 
